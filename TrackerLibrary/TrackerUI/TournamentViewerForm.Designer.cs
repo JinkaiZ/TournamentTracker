4b17bda23a18382572e6f9046b0e5bc4169860ed
JinkaiZ <Jzha541@aucklanduni.ac.nz>
2021-02-02 00:50:59 +1300

    Set up UI

--- TrackerLibrary/TrackerUI/TournamentViewerForm.Designer.cs
+++ TrackerLibrary/TrackerUI/TournamentViewerForm.Designer.cs
@@ -29,23 +29,53 @@ namespace TrackerUI
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(48, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Turnament:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.label2.Location = new System.Drawing.Point(173, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "<none>";
             // 
             // TournamentViewerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(937, 1061);
+            this.ClientSize = new System.Drawing.Size(668, 376);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
             this.Name = "TournamentViewerForm";
             this.Text = "Tournament Viewer";
+            this.Load += new System.EventHandler(this.TournamentViewerForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
