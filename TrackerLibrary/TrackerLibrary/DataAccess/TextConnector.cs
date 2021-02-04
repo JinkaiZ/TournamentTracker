0e5bde6b59beac1804a97ea217076fba616dd19d
JinkaiZ <Jzha541@aucklanduni.ac.nz>
2021-02-05 00:09:48 +1300

    Set up the text file database for creating prize form.

--- TrackerLibrary/TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/TrackerLibrary/DataAccess/TextConnector.cs
@@ -2,17 +2,43 @@
 using System.Collections.Generic;
 using System.Text;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelper;
+using System.Linq;
 
 namespace TrackerLibrary.DataAccess
 {
     class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModels.csv";
+
+
+
         // TODO - Write up the CreatePrize for text files.
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+
+            // Load the text file
+            // Convert the text to List<PrizeModel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+            // Find the ID
+            int currentId = 1;
+            if(prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+            model.Id = currentId;
+
+            // Add the new record with the new ID
+            prizes.Add(model);
+
+            // Convert the prizes to list<string>
+            // Save the list<string> to the text file
+
+            prizes.SaveToPrizesFile(PrizesFile);
 
             return model;
+
         }
     }
 }
--- TrackerLibrary/TrackerUI/CreatePrizeForm.cs
+++ TrackerLibrary/TrackerUI/CreatePrizeForm.cs
@@ -41,10 +41,9 @@ namespace TrackerUI
                     prizeAmountValue.Text,
                     prizePercentageValue.Text);
 
-                foreach (IDataConnection db in GlobalConfig.Connections)
-                {
-                    db.CreatePrize(model);
-                }
+
+                GlobalConfig.Connection.CreatePrize(model); 
+                
 
            
                 placeNameValue.Text = "";
